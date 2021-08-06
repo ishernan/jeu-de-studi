57f7a46437f4f538387df062c0c38d9df3fe4791
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-08-06 18:00:29 +0200

    JS file with functions3

--- jeu.js
+++ jeu.js
@@ -1,11 +1,15 @@
-const rejouer   = document.querySelector('#rejouer'); 
-const de1       = document.querySelector('#de1'); 
-const roulezDes = document.querySelector('#roulez-des'); 
-const points1   = document.querySelector('#points-1');
-const points2   = document.querySelector('#points-2');
-const select1   = document.querySelector('#select1');
-const select2   = document.querySelector('#select2');
-let player1, player2;
+const rejouer     = document.querySelector('#rejouer'); 
+const de         = document.querySelector('#de'); 
+const roulezDes   = document.querySelector('#roulez-des'); 
+const points1     = document.querySelector('#points-1');
+const points2     = document.querySelector('#points-2');
+const score1      = document.querySelector('#score-1')
+const score2      = document.querySelector('#score-2')
+const select1     = document.querySelector('#select1');
+const select2     = document.querySelector('#select2');
+const enregistrer = document.querySelector('#enregistrer');
+ 
+let player1, player2, puntos;
 
 
 roulezDes.addEventListener('click', ()=>{
@@ -13,9 +17,8 @@ roulezDes.addEventListener('click', ()=>{
 })
 
 rejouer.addEventListener('click', ()=>{
-  de1.style.display = 'none'; 
-  points1.textContent = 0;
-  points2.textContent = 0;
+  location.reload();
+  //de.style.display = 'none'; 
 } )
 
 select1.addEventListener('click', ()=>{
@@ -23,6 +26,8 @@ select1.addEventListener('click', ()=>{
   select2.style.backgroundColor = "white"; 
   player1 = true;
   player2 = false;
+  //points2.textContent = 0; 
+
   
 })
 select2.addEventListener('click', ()=>{
@@ -30,20 +35,42 @@ select2.addEventListener('click', ()=>{
   select1.style.backgroundColor = "white"; 
   player2 = true;
   player1 = false;
+  //points1.textContent = 0; 
+})
+
+enregistrer.addEventListener('click', ()=>{
+  if(player1 === true){
+    score1.textContent = puntos; 
+    points1.textContent = 0;
+  } else if(player2 === true){
+    score2.textContent = puntos; 
+    points2.textContent = 0;
+
+  }
+  
+  
 })
 
 function jeuDe () { 
-  let rouler1 = Math.floor((Math.random() * 6) + 1 ); 
+  let rouler = Math.floor((Math.random() * 6) + 1 ); 
 
-  de1.style.display = 'block';
-  de1.src = 'images/dice-' + rouler1 + '.png';
+  de.style.display = 'block';
+  de.src = 'images/dice-' + rouler + '.png';
   
-  if(player1 === true ){
-    points1.textContent = rouler1;     
-  } else if(player2===true){
-    points2.textContent = rouler1; 
-    }
+  if(player1 === true && rouler != 1) {
+    points1.textContent = rouler; 
+    puntos = rouler;     
+  } else if(player2===true && rouler != 1){
+    points2.textContent = rouler; 
+    puntos = rouler;     
+    } else if (player1 === true && rouler === 1){
+      points1.textContent = 0; 
+    } else if (player2 === true && rouler === 1){
+      points2.textContent = 0;
   }
+
+
+}
 
   
 
