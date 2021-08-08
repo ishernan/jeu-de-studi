99e35aa83797206161a7625e15ba2765d59a9f66
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-08-08 17:19:52 +0200

    function enregistrer, factorisation, limite

--- jeu.js
+++ jeu.js
@@ -8,70 +8,49 @@ const score2      = document.querySelector('#score-2')
 const select1     = document.querySelector('#select1');
 const select2     = document.querySelector('#select2');
 const enregistrer = document.querySelector('#enregistrer');
-const limiteFinal = document.querySelector('#scoreFinal');
+const limite      = document.querySelector('#scoreFinal');
  
-let player1, player2, puntos;
+let player1 = false;
+let player2 = false; 
+let puntos, limiteFinal, pointsCumulés1, pointsCumulés2;
 
 
-roulezDes.addEventListener('click', ()=>{
-  jeuDe(); 
-})
+roulezDes.addEventListener('click', jeuDe)
 
 rejouer.addEventListener('click', ()=>{
   location.reload();
   //de.style.display = 'none'; 
 } )
+select1.addEventListener('click', selectOne);
+select2.addEventListener('click', selectTwo); 
+enregistrer.addEventListener('click', enregistrerPoints); 
 
-select1.addEventListener('click', ()=>{
-  selectOne();
-  // select1.style.backgroundColor = "#f1a19e"; 
-  // select2.style.backgroundColor = "white"; 
-  // player1 = true;
-  // player2 = false;
 
-  
-})
-select2.addEventListener('click', ()=>{
-  selectTwo();
-  // select2.style.backgroundColor = "#f1a19e"; 
-  // select1.style.backgroundColor = "white"; 
-  // player2 = true;
-  // player1 = false;
-})
-
-enregistrer.addEventListener('click', ()=>{
-  if(player1 === true){
-    score1.textContent = puntos; 
-    points1.textContent = 0;
-  } else if(player2 === true){
-    score2.textContent = puntos; 
-    points2.textContent = 0;
-
-  }
-    
-})
 
 function jeuDe () { 
   let rouler = Math.floor((Math.random() * 6) + 1 ); 
 
   de.style.display = 'block';
   de.src = 'images/dice-' + rouler + '.png';
-  
-  if(player1 === true && rouler != 1) {
+
+  if(player1 === false && player2 === false){
+    de.style.display = 'none'; 
+    return alert('Choisir un joueur !');  
+  } else if (player1 === true && rouler != 1) {
     points1.textContent = rouler; 
     puntos = rouler;     
   } else if(player2===true && rouler != 1){
     points2.textContent = rouler; 
     puntos = rouler;     
-    } else if (player1 === true && rouler === 1){
-      points1.textContent = 0; 
-      //cambiar de jugador
-    } else if (player2 === true && rouler === 1){
+  } else if (player1 === true && rouler === 1){
+    points1.textContent = 0; 
+    selectTwo(); 
+  } else if (player2 === true && rouler === 1){
       points2.textContent = 0;
-     //cambiar de jugador
-
+      selectOne(); 
   }
-
+  
+  limiteFinal = limite.value; 
 
 }
 
@@ -89,14 +68,25 @@ function selectTwo(){
     player1 = false;
   }
   
+function limiteGagnant(){
+  if(puntos >= limiteFinal){
+   return console.log("vous avez gagné");
+  }
 
-// function limiteFinal(){
-//   limiteFinal.inputValue = 
-//   if(limiteFinal >= score1){
+}
 
-//   }
-// }
-  
+function enregistrerPoints(){
+  if(player1 === true){
+    score1.textContent = puntos; 
+    points1.textContent = 0;
+    pointsCumulés1 += puntos;  
+  } else if(player2 === true){
+    score2.textContent = puntos; 
+    points2.textContent = 0;
+    pointsCumulés2 += puntos;  
 
+  }
+  limiteGagnant();
+}
  
  
