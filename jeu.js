506455e64f84bc19c9df8d1090c21c0af31caf7c
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-08-09 16:12:26 +0200

    limite final

--- jeu.js
+++ jeu.js
@@ -1,5 +1,5 @@
 const rejouer     = document.querySelector('#rejouer'); 
-const de         = document.querySelector('#de'); 
+const de          = document.querySelector('#de'); 
 const roulezDes   = document.querySelector('#roulez-des'); 
 const points1     = document.querySelector('#points-1');
 const points2     = document.querySelector('#points-2');
@@ -8,12 +8,11 @@ const score2      = document.querySelector('#score-2')
 const select1     = document.querySelector('#select1');
 const select2     = document.querySelector('#select2');
 const enregistrer = document.querySelector('#enregistrer');
-const limite      = document.querySelector('#scoreFinal');
+const limite      = document.querySelector('#limiteFinal');
  
 let player1 = false;
 let player2 = false; 
 let puntos, limiteFinal, pointsCumulés1, pointsCumulés2;
-
 
 roulezDes.addEventListener('click', jeuDe)
 
@@ -25,18 +24,22 @@ select1.addEventListener('click', selectOne);
 select2.addEventListener('click', selectTwo); 
 enregistrer.addEventListener('click', enregistrerPoints); 
 
-
-
 function jeuDe () { 
   let rouler = Math.floor((Math.random() * 6) + 1 ); 
 
   de.style.display = 'block';
   de.src = 'images/dice-' + rouler + '.png';
-
+  limiteFinal = limite.value; 
+  
   if(player1 === false && player2 === false){
     de.style.display = 'none'; 
     return alert('Choisir un joueur !');  
-  } else if (player1 === true && rouler != 1) {
+  } 
+  else if (limiteFinal <= 0){
+    return alert('vous devez préciser une limite valide'); 
+  }  
+  else if (player1 === true && rouler != 1) {
+
     points1.textContent = rouler; 
     puntos = rouler;     
   } else if(player2===true && rouler != 1){
@@ -49,8 +52,6 @@ function jeuDe () {
       points2.textContent = 0;
       selectOne(); 
   }
-  
-  limiteFinal = limite.value; 
 
 }
 
@@ -68,25 +69,23 @@ function selectTwo(){
     player1 = false;
   }
   
-function limiteGagnant(){
-  if(puntos >= limiteFinal){
-   return console.log("vous avez gagné");
-  }
-
-}
-
 function enregistrerPoints(){
   if(player1 === true){
-    score1.textContent = puntos; 
+    pointsCumulés1 =  puntos;  
+    score1.textContent = pointsCumulés1; 
     points1.textContent = 0;
-    pointsCumulés1 += puntos;  
+    
   } else if(player2 === true){
     score2.textContent = puntos; 
     points2.textContent = 0;
-    pointsCumulés2 += puntos;  
 
-  }
-  limiteGagnant();
+
+    console.log(limiteFinal)
+
+  } else if(score1 >= limiteFinal){
+    return alert("vous avez gagné");
+   }
+  
 }
  
  
